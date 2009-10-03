92bd5fc330e2a48ae84fc185f3e397aefb4be1eb
Harry Pierson <harry.pierson@microsoft.com>
2009-10-02 17:16:45 -0700

    Initial checkin of simple Things form + Python script to get things

---
+++ LightweightDebuggerDemo/GetThings.py
@@ -0,0 +1,17 @@
+﻿import clr
+clr.AddReference("System.Xml")
+from System.Xml import XmlDocument
+
+def get_nodes(xml):
+	return xml.SelectNodes("statuses/status/text")
+
+def download_stuff():
+	x = XmlDocument()
+
+	x.Load("devhawk.xml")
+
+	for n in get_nodes(x):
+		txt = n.InnerText
+		items.Add(txt)
+		
+download_stuff()
---
+++ LightweightDebuggerDemo/Things.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using IronPython.Hosting;
+
+namespace LightweightDebuggerDemo
+{
+    /// <summary>
+    /// Interaction logic for Things.xaml
+    /// </summary>
+    public partial class Things : Window
+    {
+        public Things()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var engine = Python.CreateEngine();
+
+            var s = engine.CreateScope();
+            s.SetVariable("items", lbThings.Items);
+            engine.ExecuteFile("getthings.py", s);
+        }
+    }
+}
